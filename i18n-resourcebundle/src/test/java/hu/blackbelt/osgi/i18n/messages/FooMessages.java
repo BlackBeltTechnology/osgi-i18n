eb9f6bf08591c25ceb6a355629d3f0b9c4f173bd
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:42:35 +0100

    Fix tabs and line ends

--- i18n-resourcebundle/src/test/java/hu/blackbelt/osgi/i18n/messages/FooMessages.java
+++ i18n-resourcebundle/src/test/java/hu/blackbelt/osgi/i18n/messages/FooMessages.java
@@ -9,9 +9,9 @@ package hu.blackbelt.osgi.i18n.messages;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,13 +26,13 @@ import hu.blackbelt.osgi.i18n.api.MessageByKey;
 public interface FooMessages {
 
     String test_None();
-    
+
     String test_String(String s);
-    
+
     String test_byte(byte s);
 
     String test_Byte(Byte s);
-    
+
     String test_short(short s);
 
     String test_Short(Short s);
@@ -40,25 +40,25 @@ public interface FooMessages {
     String test_int(int i);
 
     String test_Integer(Integer i);
-    
+
     String test_long(long i);
 
     String test_Long(Long i);
-    
+
     String test_float(float i);
 
     String test_Float(Float i);
-    
+
     String test_double(double i);
 
     String test_Double(Double i);
-    
+
     String test_char(char i);
 
     String test_Character(Character i);
-    
+
     String test_Object(Object i);
-    
+
     String test_2param(String p1, int p2);
 
     String test_3param(String p1, int p2, String p3);
